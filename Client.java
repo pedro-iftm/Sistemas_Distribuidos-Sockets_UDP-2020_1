4662b38edd2dc10f20bbae2fc47ec6cdc28d81cd
Pedro Santos Arruda <pedro.arruda@morpheustecnologia.com>
2020-05-04 19:54:21 -0300

    Remove comments

--- Client.java
+++ Client.java
@@ -4,13 +4,11 @@ import java.net.DatagramSocket;
 import java.net.InetAddress;
 import java.util.Scanner;
 
-public class Cliente {
+public class Client {
 	
 	public static void main(String[] args) throws IOException {
 		
 		Scanner sc = new Scanner(System.in);
-		
-		// Passo 1: Criar o objeto socket que é responsável por manter o dado
 		DatagramSocket ds = new DatagramSocket();
 		
 		InetAddress ip = InetAddress.getLocalHost();
@@ -19,14 +17,8 @@ public class Cliente {
 		while (true)
 		{
 			String inp = sc.nextLine();
-			
-			// Converter a string input em um array byte
 			buf = inp.getBytes();
-			
-			// Passo 2: Criar o datagramPacket responsável por enviar o dado.
 			DatagramPacket DpSend = new DatagramPacket(buf, buf.length, ip, 1234);
-			
-			// Passo 3: Enviar o dado
 			ds.send(DpSend);
 			
 			if (inp.equals("bye"))
